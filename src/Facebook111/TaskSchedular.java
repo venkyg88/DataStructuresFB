6b401e16f5dd02b857c576d180da7810f2c8eebe
Venkat Gonuguntala <venkyg88@gmail.com>
2017-10-11 22:12:12 -0500

    find the frequency of the tasks
    (c[25] - 1) * (n + 1) + 25 - i)

---
+++ src/Facebook111/TaskSchedular.java
@@ -0,0 +1,17 @@
+// (c[25] - 1) * (n + 1) + 25 - i  is frame size
+// when inserting chars, the frame might be "burst", then tasks.length takes precedence
+// when 25 - i > n, the frame is already full at construction, the following is still valid.
+public class TaskSchedular {
+    public int leastInterval(char[] tasks, int n) {
+
+        int[] c = new int[26];
+        for(char t : tasks){
+            c[t - 'A']++;
+        }
+        Arrays.sort(c);
+        int i = 25;
+        while(i >= 0 && c[i] == c[25]) i--;
+
+        return Math.max(tasks.length, (c[25] - 1) * (n + 1) + 25 - i);
+    }
+}
