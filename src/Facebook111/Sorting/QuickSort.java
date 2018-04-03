dcf735379c3b014132457583d16f95cbde430d97
Venkat Gonuguntala <venkyg88@gmail.com>
2018-04-02 19:18:38 -0500

    Create QuickSort.java

---
+++ src/Facebook111/Sorting/QuickSort.java
@@ -0,0 +1,38 @@
+
+Time complexity: O(nlogn) . Worst case: O(n2)
+Space Complexity: O(logn)
+
+
+public class QuickSort{
+  public static void quicksort(int array[]){
+    quicksort(array, 0, array.length - 1);
+  }
+  
+  public static void quicksort(int array[], int left, int right) {
+    if(left >= right) {
+      return;
+    }
+    int pivot = array[(left+right)/2];
+    int index = partition(array, left, right, pivot);
+    quicksort(array, left, index - 1);
+    quicksort(array, index, right);
+  }
+  
+  public static void partition(int[] array, int left, int right, int pivot){
+    while (left <= right) {
+      while(array[left] <= pivot){
+        left++;
+      }
+      while(array[right]>= pivot) {
+        right--;
+      }
+      
+      if(left<=right){
+        swap(array,left, right);
+        left++;
+        right--;
+      }
+    }
+    return left;
+  }
+}
