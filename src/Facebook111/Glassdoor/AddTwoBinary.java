841249c1a78cc2c52de5bf291102c5e186e30ca4
Venkat Gonuguntala <venkyg88@gmail.com>
2018-04-17 13:55:25 -0500

    Update AddTwoBinary.java

--- src/Facebook111/Glassdoor/AddTwoBinary.java
+++ src/Facebook111/Glassdoor/AddTwoBinary.java
@@ -1,41 +1,50 @@
-public class AddTwoBinary{
-    public String addBinary(String a, String b) {
-    StringBuilder sb = new StringBuilder();
- 
-    int i=a.length()-1;
-    int j=b.length()-1;
- 
-    int carry = 0;
- 
-    while(i>=0 || j>=0){
-        int sum=0;
- 
-        if(i>=0 && a.charAt(i)=='1'){
-            sum++;    
-        }
- 
-        if(j>=0 && b.charAt(j)=='1'){
-            sum++;
-        }
- 
-        sum += carry;
- 
-        if(sum>=2){
-            carry=1;
-        }else{
-            carry=0;
-        }
- 
-        sb.insert(0,  (char) ((sum%2) + '0'));
- 
-        i--;
-        j--;
+public class AddTwoBinary {
+    public static void main(String args[]) {
+        
+        //add two strings
+        String s1 = "111";
+        String s2 = "101";
+        MyClass a = new MyClass();
+        System.out.println("Values "+a.addedStrings(s1, s2));
     }
+    
+    public String addedStrings(String a, String b) {
+        StringBuilder sb = new StringBuilder();
  
-    if(carry==1)
-        sb.insert(0, '1');
+        int i=a.length()-1;
+        int j=b.length()-1;
  
-    return sb.toString();
-  }
-
+        int carry = 0;
+ 
+        while(i>=0 || j>=0){
+            int sum=0;
+ 
+            if(i>=0 && a.charAt(i)=='1'){
+                sum++;    
+            }
+ 
+            if(j>=0 && b.charAt(j)=='1'){
+                sum++;
+            }
+ 
+            sum += carry;
+ 
+            if(sum>=2){
+                carry=1;
+            }else{
+                carry=0;
+            }
+ 
+            sb.insert(0,  (char) ((sum%2) + '0'));
+ 
+            i--;
+            j--;
+        }
+ 
+        if(carry==1)
+            sb.insert(0, '1');
+ 
+        return sb.toString();
+  
+    }
 }
