f98fd4ba4aebeb4b3f3fd5c70c8dabce7cd3a040
Venkat Gonuguntala <venkyg88@gmail.com>
2018-11-14 15:48:14 -0600

    Maximum sum of continuous array elements

---
+++ src/Arrays/MaxSubArraySum.java
@@ -0,0 +1,18 @@
+public class MaxSubArraySum {
+  public static void main(String[] args){
+      int[] a = {-2,1,3,-3,2,4,-2,6};
+      System.out.println(maxSubArraySum(a));
+  }
+  
+  public static int maxSubArraySum(int[] a) {
+      int max_so_far = -100;
+      int max_ending = 0;
+      
+      for( int i=0; i< a.length; i++){
+        max_ending = max_so_far + a[i];
+        if( max_so_far < max_ending) max_so_far = max_ending;
+        if( max_ending < 0 ) max_ending = 0;
+      }
+      return max_so_far;
+  }
+}
